c79f795a96af79e16da222c73325c5580f770db2
Leire <leirehe@gmail.com>
2014-09-30 12:52:33 +0200

    develop commit 2

--- src/main/java/ejercicio/Entrega.java
+++ src/main/java/ejercicio/Entrega.java
@@ -2,13 +2,16 @@ package ejercicio;
 
 public class Entrega {
 
-    public void ejercicio (){
-        
+    public void ejercicio() {
+
     }
-    
-    public void ejercicio1(){
-        
-        
+
+    public void ejercicio1() {
+
     }
-    
+
+    public void ejercici2() {
+
+    }
+
 }
